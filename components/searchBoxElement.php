afbea85df46bfe074169b5c239c77a315c25a71d
Ivo Petkov <ivo@ivopetkov.com>
2019-12-12 13:40:36 +0200

    Added container element class attribute.

--- components/searchBoxElement.php
+++ components/searchBoxElement.php
@@ -7,7 +7,7 @@ $app = App::get();
 echo '<html>';
 echo '<body>';
 
-echo '<div style="position:relative;">';
+echo '<div class="bearcms-search-box-element" style="position:relative;">';
 $buttonOnClick = 'window.location.href="' . $app->urls->get('/s/') . '"+encodeURIComponent(this.nextSibling.value)+"/";';
 echo '<a title="' . htmlentities(__('bearcms/search-box-element-addon/ButtonTitle')) . '" onclick="' . htmlentities($buttonOnClick) . '" class="bearcms-search-box-element-button"></a>';
 echo '<input onkeyup="if(event.keyCode==13){this.previousSibling.click();}" class="bearcms-search-box-element-input" autocomplete="off" placeholder="' . htmlentities(__('bearcms/search-box-element-addon/ButtonTitle')) . '"/>';
