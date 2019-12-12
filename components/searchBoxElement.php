246b39ed5792328e48c6e4808d09fabbbb4dcfbd
Ivo Petkov <ivo@ivopetkov.com>
2019-12-12 12:59:00 +0200

    Added title attribute to the search button.

--- components/searchBoxElement.php
+++ components/searchBoxElement.php
@@ -9,8 +9,8 @@ echo '<body>';
 
 echo '<div style="position:relative;">';
 $buttonOnClick = 'window.location.href="' . $app->urls->get('/s/') . '"+encodeURIComponent(this.nextSibling.value)+"/";';
-echo '<a onclick="' . htmlentities($buttonOnClick) . '" class="bearcms-search-box-element-button"></a>';
-echo '<input onkeyup="if(event.keyCode==13){this.previousSibling.click();}" class="bearcms-search-box-element-input" autocomplete="off" placeholder="' . __('bearcms/search-box-element-addon/ButtonTitle') . '"/>';
+echo '<a title="' . htmlentities(__('bearcms/search-box-element-addon/ButtonTitle')) . '" onclick="' . htmlentities($buttonOnClick) . '" class="bearcms-search-box-element-button"></a>';
+echo '<input onkeyup="if(event.keyCode==13){this.previousSibling.click();}" class="bearcms-search-box-element-input" autocomplete="off" placeholder="' . htmlentities(__('bearcms/search-box-element-addon/ButtonTitle')) . '"/>';
 echo '</div>';
 
 echo '</body>';
