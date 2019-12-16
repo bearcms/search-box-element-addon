619d433f4afccf5b4c82542644226125e997578b
Ivo Petkov <ivo@ivopetkov.com>
2019-12-16 16:29:30 +0200

    The search button will focus the input if empty.

--- components/searchBoxElement.php
+++ components/searchBoxElement.php
@@ -8,7 +8,7 @@ echo '<html>';
 echo '<body>';
 
 echo '<div class="bearcms-search-box-element" style="position:relative;">';
-$buttonOnClick = 'window.location.href="' . $app->urls->get('/s/') . '"+encodeURIComponent(this.nextSibling.value)+"/";';
+$buttonOnClick = 'var q=this.nextSibling.value;if(q.length>0){window.location.href="' . $app->urls->get('/s/') . '"+encodeURIComponent(q)+"/";}else{this.nextSibling.focus();}';
 echo '<a title="' . htmlentities(__('bearcms/search-box-element-addon/ButtonTitle')) . '" onclick="' . htmlentities($buttonOnClick) . '" class="bearcms-search-box-element-button"></a>';
 echo '<input onkeyup="if(event.keyCode==13){this.previousSibling.click();}" class="bearcms-search-box-element-input" autocomplete="off" placeholder="' . htmlentities(__('bearcms/search-box-element-addon/ButtonTitle')) . '"/>';
 echo '</div>';
